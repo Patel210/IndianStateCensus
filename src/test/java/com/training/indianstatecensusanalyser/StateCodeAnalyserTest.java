3b8782f09969b70af88d058b1bdc002757a0a426
Patel210 <pranavpatel2101997@gmail.com>
2020-10-17 00:14:11 +0530

    Refactored test cases to include CSV Exception from another package and separate test files created for 2 different methods in the analyser class

--- src/test/java/com/training/indianstatecensusanalyser/StateCodeAnalyserTest.java
+++ src/test/java/com/training/indianstatecensusanalyser/StateCodeAnalyserTest.java
@@ -1,15 +1,14 @@
 package com.training.indianstatecensusanalyser;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import com.training.csvbuilder.*;
+import com.training.csvbuilder.CSVException;
 
-public class StateCensusAnalyserTest {
-
+public class StateCodeAnalyserTest {
 	private static final String STATE_CENSUS_FILE_PATH = "C:\\Users\\Pranav\\eclipse-workspace\\IndianStateCensusAnalyser\\src\\test\\resources\\IndianStateCensusData.csv";
 	private static final String INCORRECT_STATE_CENSUS_FILE_PATH = "C:\\Users\\Pranav\\eclipse-workspace\\IndianStateCensusAnaylser\\src\\main\\resources\\IndianStateCensusData.csv";
 	private static final String INCORRECT_TYPE_STATE_CENSUS_FILE_PATH = "C:\\Users\\Pranav\\eclipse-workspace\\IndianStateCensusAnalyser\\src\\test\\resources\\IndianStateCensusDataWithIncorrectType.txt";
@@ -25,47 +24,46 @@ public class StateCensusAnalyserTest {
 		exceptionRule.expect(CSVException.class);
 	}
 	
-	
 	@Test
-	public void givenCensusFileShouldReturnCorrectNumberOfEnteries() {
+	public void givenStateCensusFileWhenProcessedForStateCodeShouldReturnCorrectNumberOfEnteries() {
 		try {
-			int entries = stateCensusAnalyser.loadStateCensusData(STATE_CENSUS_FILE_PATH);
+			int entries = stateCensusAnalyser.loadStateCodeData(STATE_CENSUS_FILE_PATH);
 			assertEquals(36, entries);
 		} catch (CSVException e) {
 		}
 	}
-
+	
 	@Test
-	public void givenWrongFilePathShouldThrowAnException() {
+	public void givenWrongCensusFilePathWhenProcessedForStateCodeShouldThrowAnException() {
 		try {
-			stateCensusAnalyser.loadStateCensusData(INCORRECT_STATE_CENSUS_FILE_PATH);
+			stateCensusAnalyser.loadStateCodeData(INCORRECT_STATE_CENSUS_FILE_PATH);
 		} catch (CSVException e) {
 			assertEquals(CSVException.ExceptionType.FILE_PROBLEM, e.type);
 		}
 	}
-
+	
 	@Test
-	public void givenWrongFileTypeWhenProcessedShouldThrowAnException() {
+	public void givenWrongCensusFileTypeWhenProcessedForStateCodeShouldThrowAnException() {
 		try {
-			stateCensusAnalyser.loadStateCensusData(INCORRECT_TYPE_STATE_CENSUS_FILE_PATH);
+			stateCensusAnalyser.loadStateCodeData(INCORRECT_TYPE_STATE_CENSUS_FILE_PATH);
 		} catch (CSVException e) {
 			assertEquals(CSVException.ExceptionType.INCORRECT_TYPE, e.type);
 		}
 	}
-
+	
 	@Test
-	public void givenFileWithWrongDelimiterWhenProcessedShouldThrowAnException() {
+	public void givenCensusFileWithWrongDelimiterWhenProcessedForStateCodeShouldThrowAnException() {
 		try {
-			stateCensusAnalyser.loadStateCensusData(INCORRECT_DELIMITER_STATE_CENSUS_FILE_PATH);
+			stateCensusAnalyser.loadStateCodeData(INCORRECT_DELIMITER_STATE_CENSUS_FILE_PATH);
 		} catch (CSVException e) {
 			assertEquals(CSVException.ExceptionType.INCORRECT_DELIMITER, e.type);
 		}
 	}
 	
 	@Test
-	public void givenFileWithWrongHeaderWhenProcessedShouldThrowAnException() {
+	public void givenCensusFileWithWrongHeaderWhenProcessedForStateCodeShouldThrowAnException() {
 		try {
-			stateCensusAnalyser.loadStateCensusData(INCORRECT_HEADER_STATE_CENSUS_FILE_PATH);
+			stateCensusAnalyser.loadStateCodeData(INCORRECT_HEADER_STATE_CENSUS_FILE_PATH);
 		} catch (CSVException e) {
 			assertEquals(CSVException.ExceptionType.INCORRECT_HEADER, e.type);
 		}
