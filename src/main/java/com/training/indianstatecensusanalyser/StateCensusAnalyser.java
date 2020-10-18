53bbeadcc662c01d5dfc91288febf419fb237c73
Patel210 <pranavpatel2101997@gmail.com>
2020-10-18 11:10:26 +0530

    Refactored the code to return List in place of iterator and modified the openCSV jar to achieve this result

--- src/main/java/com/training/indianstatecensusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/training/indianstatecensusanalyser/StateCensusAnalyser.java
@@ -5,6 +5,7 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 import opencsvbuilder.CSVBuilderFactory;
@@ -16,8 +17,8 @@ public class StateCensusAnalyser {
 	public int loadStateCensusData(String filePath) throws CSVException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(filePath));) {
 			ICSVBuilder<CSVStateCensus> csvBuilder = CSVBuilderFactory.createCSVBuilder();
-			Iterator<CSVStateCensus> iterator = csvBuilder.getCSVFileIterator(reader, CSVStateCensus.class, filePath);
-			return getCount(iterator);
+			List<CSVStateCensus> stateCensusList = csvBuilder.getCSVFileList(reader, CSVStateCensus.class, filePath);
+			return stateCensusList.size();
 		} catch (IOException e) {
 			throw new CSVException("File Problem encountered", CSVException.ExceptionType.FILE_PROBLEM);
 		}
@@ -26,15 +27,10 @@ public class StateCensusAnalyser {
 	public int loadStateCodeData(String filePath) throws CSVException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(filePath));) {
 			ICSVBuilder<CSVStates> csvBuilder = CSVBuilderFactory.createCSVBuilder();
-			Iterator<CSVStates> iterator = csvBuilder.getCSVFileIterator(reader, CSVStates.class, filePath);
-			return getCount(iterator);
+			List<CSVStates> stateCodeList = csvBuilder.getCSVFileList(reader, CSVStates.class, filePath);
+			return stateCodeList.size();
 		} catch (IOException e) {
 			throw new CSVException("File problem encountered", CSVException.ExceptionType.FILE_PROBLEM);
 		}
-	}
-	
-	private <E> int getCount(Iterator<E> iterator) {
-		Iterable<E> csvIterable = () -> iterator;
-		return (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
 	}
 }
